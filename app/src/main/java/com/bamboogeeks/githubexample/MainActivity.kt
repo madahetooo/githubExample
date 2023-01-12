f9adb4734405528621f7a9088bf7ac4d6bd87fb2
madahetooo <ieslammedhat@gmail.com>
2023-01-12 22:22:35 +0200

    forth reels commit

--- app/src/main/java/com/bamboogeeks/githubexample/MainActivity.kt
+++ app/src/main/java/com/bamboogeeks/githubexample/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         print("Stories Commit")
         print("Reels Commit")
+        print("Reels2 Commit")
+        print("Reels3 Commit")
     }
 }
