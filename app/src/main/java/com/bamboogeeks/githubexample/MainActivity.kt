edbf9b9fdad6180bfc941018457cadcb87575f64
madahetooo <ieslammedhat@gmail.com>
2023-01-12 22:11:54 +0200

    another stories commit

--- app/src/main/java/com/bamboogeeks/githubexample/MainActivity.kt
+++ app/src/main/java/com/bamboogeeks/githubexample/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("Stories Commit")
+        print("Another Stories Commit")
     }
 }
